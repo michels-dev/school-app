3c9436f36d9914f9a87f7e5475f1460ed00cf827
michelsinambela <sinambelamichel05@gmail.com>
2023-08-25 12:05:36 +0700

    Kommit Kedua

--- public/template/dist/js/demo.js
+++ public/template/dist/js/demo.js
@@ -14,7 +14,7 @@
     if (window.___browserSync___ === undefined && Number(localStorage.getItem('AdminLTE:Demo:MessageShowed')) < Date.now()) {
       localStorage.setItem('AdminLTE:Demo:MessageShowed', (Date.now()) + (15 * 60 * 1000))
       // eslint-disable-next-line no-alert
-      alert('Selamat Datang Di Halaman Web Prediksi Penderita Penyakit Stroke')
+      alert('Selamat Datang Di Halaman Web School App Sederhana')
     }
   }, 1000)
 
